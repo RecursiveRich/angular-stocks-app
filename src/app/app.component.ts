d20fb454cfd7164dc36cd88650e0d2c660709f74
RICHARD C BERBERIAN <humblemind@hotmail.com>
2018-09-09 12:29:47 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+// import { StocksService, StockInterface } from "./services/stocks.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'stocks';
-}
+export class AppComponent {}
+
+// stocks: Array<StockInterface>;
+
+// constructor(service: StocksService) {
+//   service.load(["AAPL"]).subscribe(stocks => {
+//     this.stocks = stocks;
+//   });
+// }
+
+// title = "stocks";
